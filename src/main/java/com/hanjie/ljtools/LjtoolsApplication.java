6979e12a570cb06ab3008f540e907d9dd65c2b77
hanjie666 <13553232959@163.com>
2020-04-26 19:22:59 +0800

    增加hello注释

--- src/main/java/com/hanjie/ljtools/LjtoolsApplication.java
+++ src/main/java/com/hanjie/ljtools/LjtoolsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class LjtoolsApplication extends SpringBootServletInitializer {
-
+    //hello
     public static void main(String[] args) {
         SpringApplication.run(LjtoolsApplication.class, args);
     }
